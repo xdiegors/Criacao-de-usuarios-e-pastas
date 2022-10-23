9f30f1b5a95c8fca949ca012a4f40572dda583b3
xdiegors <xdiegors@outlook.com>
2022-10-23 11:12:02 -0300

    Finalizado

---
+++ criacaoDeUsuariosEpastas.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Pastas criadas"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Grupos criados"
+
+useradd carlos -m -s /bin/bash -p padrao -G GRP_ADM
+useradd maria -m -s /bin/bash -p padrao -G GRP_ADM
+useradd joao -m -s /bin/bash -p padrao -G GRP_ADM
+useradd debora -m  -s /bin/bash -p padrao -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p padrao -G GRP_VEN
+useradd roberto -m -s /bin/bash -p padrao -G GRP_VEN
+useradd josefina -m -s /bin/bash -p padrao -G GRP_SEC
+useradd amanda -m -s /bin/bash -p padrao -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p padrao -G GRP_SEC
+
+echo "Usuários criados"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Permissões atribuídas às pastas"
+
